6cbd716673c12a98f5683576c17972fff07b851c
Monir <ahmadmonirosmani@gmail.com>
2022-11-01 10:55:26 +0100

    Section component created

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,14 @@ import Section from "./Section";
 function Home() {
   return (
     <Container>
-      <Section />
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Cutom order"
+        rightBtnText="Existing Inventory"
+      />
+      {/* <Section /> */}
     </Container>
   );
 }
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 
-function Section() {
-  return <Wrap>Section</Wrap>;
+function Section({ title, description, leftBtnText, rightBtnText }) {
+  return (
+    <Wrap>
+      <ItemText>
+        <h1>{title}</h1>
+        <p style={{ padding: `20px` }}>{description}</p>
+      </ItemText>
+      <Buttons>
+        <ButtonContainer>
+          <LeftButton>{leftBtnText}</LeftButton>
+          <RightButton>{rightBtnText}</RightButton>
+        </ButtonContainer>
+        <DownArrow src="/images/down-arrow.svg"></DownArrow>
+      </Buttons>
+    </Wrap>
+  );
 }
 
 export default Section;
@@ -10,6 +24,61 @@ export default Section;
 const Wrap = styled.div`
   width: 100vw;
   height: 100vh;
-  background: orange;
-  background-image: url();
+  background-image: linear-gradient(180deg, rgba(250, 250, 250, 0) 80%, #000000 110%),
+    url("/images/model-3.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  /* background-color: #fafafa; */
+  /* background-image: ; */
+`;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const Buttons = styled.div`
+  text-align: center;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+  z-index: 100;
+`;
+const RightButton = styled(LeftButton)`
+  background-color: white;
+  color: black;
+  opacity: 0.65;
+`;
+
+const DownArrow = styled.img`
+  margin-top: 20px;
+  height: 40px;
+  overflow-x: hidden;
+  animation: animateDown infinite 1.5s;
 `;
